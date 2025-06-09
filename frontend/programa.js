b6bbcd605489921d6ea7b627b63cdc87f9363115
Varcryro <criss.vngs395@gmail.com>
2025-06-09 15:53:01 -0500

    3.1

--- frontend/programa.js
+++ frontend/programa.js
@@ -1,7 +1,14 @@
-var map = L.map('map').setView([4.628007164920614, -74.06594569060938], 13);
+var map = L.map('map').setView([4.628039879669676, -74.06591030850961], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var circle = L.circle([4.628039879669676, -74.06591030850961], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 200
 }).addTo(map);
 
 // Manejar el evento de hacer click en el mapa
