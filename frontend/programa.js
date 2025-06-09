9f0f073f1785e663ad1e9970e92d6264e5bd6ce1
Varcryro <criss.vngs395@gmail.com>
2025-06-09 15:35:03 -0500

    0.2

---
+++ frontend/programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.628028537099759, -74.06594444747084], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
